ecf402af24f8e9673ccd94e9458155b201d5fc3f
paw.frolow <paw.frolow@gmail.com>
2021-09-10 09:45:02 +0300

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# cordova-plugin-check-huawei-services-google-priority
-This plugin based cordova-plugin-check-huawei-services, and add priority check for google services
+Cordova Check Huawei Services Availability
+=======================================
+
+Use this plugin to check if an android device has the `Huawei Services` available. It would be helpful if you want to check if the device is a Huawei without `Google Services`.
+
+
+## Install
+
+```
+cordova plugin add https://github.com/pawfrolow/cordova-plugin-check-huawei-services-google-priority
+```
+or
+```
+cordova plugin add cordova-plugin-check-huawei-services-google-priority
+```
+
+## Usage in Javascript
+
+```
+// available => iOS + Android
+
+CheckHuaweiServices.checkHuaweiServicesAvailability(successCallback, failureCallback)
+```
+
+- `successCallback({ status: boolean})` status will be `true` if the device is a Huawei device without Google Services.
+- `failureCallback(error:string)` is called if there was an error checking if the device has or not Google Services.
---
+++ src/android/com/checkhuaweiservices/CheckHuaweiServices.java
@@ -0,0 +1,52 @@
+package com.checkhuaweiservices;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+
+import com.google.android.gms.common.GoogleApiAvailabilityLight;
+
+import com.huawei.hms.api.ConnectionResult;
+import com.huawei.hms.api.HuaweiApiAvailability;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class CheckHuaweiServices extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("checkHuaweiServicesAvailability")) {
+            Context context = this.cordova.getActivity().getApplicationContext();
+            this.checkHuaweiServicesAvailability(context, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void checkHuaweiServicesAvailability(Context context, CallbackContext callbackContext) {
+        try {
+            JSONObject json = new JSONObject();
+            if (GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(context) == com.google.android.gms.common.ConnectionResult.SUCCESS) {
+              json.put("status", false);
+              callbackContext.success(json);
+              return;
+            }
+
+            if (HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(context) == ConnectionResult.SUCCESS) {
+              json.put("status", true);
+            } else {
+                json.put("status", false);
+            }
+
+            callbackContext.success(json);
+        } catch (Exception error) {
+            callbackContext.error("Error validating huawei services availability");
+        }
+    }
+}
